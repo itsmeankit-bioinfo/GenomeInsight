6632061adeddc619e501303f3e1813cdc732b08d
itsmeankit-bioinfo <simplyankitraj@gmail.com>
2026-07-13 14:34:27 +0530

    Configure pytest test discovery

--- test_fasta.py
+++
@@ -1,6 +0,0 @@
-from genomeinsight.io.fasta import read_fasta
-
-sequences = read_fasta("examples/sample.fasta")
-
-for seq in sequences:
-    print(seq)
--- test_fastq.py
+++
@@ -1,6 +0,0 @@
-from genomeinsight.io.fastq import read_fastq
-
-reads = read_fastq("examples/sample.fastq")
-
-for read in reads:
-    print(read)
